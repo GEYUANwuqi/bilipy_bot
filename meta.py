02fb676017f478480a82d5cd9f9e372708bb17a9
GEYUANwuqi <17539198883@163.com>
2026-02-26 16:02:54 +0800

    feat: CD工作流和meta

---
+++ meta.py
@@ -0,0 +1,12 @@
+
+__version__ = "3.0.1"
+
+__author__ = "GEYUANwuqi"
+__author_email__ = "wuqichan@outlook.com"
+__license__ = "GPL-3.0 license"
+__copyright__ = "Copyright (c) 2025 GEYUANwuqi"
+
+__maintainer__ = "GEYUANwuqi"
+__maintainer_email__ = "wuqichan@outlook.com"
+
+__summary__ = "基于事件驱动的机器人框架"
